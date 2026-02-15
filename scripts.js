6bad4afac34bb7a5b0e82e66ccd8c80a118437c7
cia2003 <graciansamosir@gmail.com>
2026-02-15 12:36:48 +0700

    update carousel

---
+++ scripts.js
@@ -0,0 +1,73 @@
+const userData = {
+    0: {
+        backgroundImage: "https://www.fpclegal.pt/xms/img/800x/39ced/cT05MCZmPXdlYnAmZmx0cltdPQ/L08zbS8tME0zWnJTbS93R3VNa1pzU21LakZSTS1abS9LWjdabk1KWnNaRVNEUy1qdHp0ZGty.webp",
+        testimonialText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        testimonialAuthor: "Catarina Azevêdo", 
+        testimonialJob: "Associate Attorney at FPC Legal"
+    }, 
+    1: {
+        backgroundImage: "https://www.fpclegal.pt/xms/img/800x/9c865/cT05MCZmPXdlYnAmZmx0cltdPQ/L08zbS8tME0zWnJTbS93R3VNa1pzU21LakZSTS1abS9LWjdabk1KWnMzWi1aRlNKanR6dGRrcg.webp",
+        testimonialText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        testimonialAuthor: "Catarina Cunha Madaleno",
+        testimonialJob: "Associate Attorney at FPC Legal"
+    }, 
+    2: {
+        backgroundImage: "https://www.fpclegal.pt/xms/img/800x/9c4e9/cT05MCZmPXdlYnAmZmx0cltdPQ/L08zbS8tME0zWnJTbS93R3VNa1pzU21LakZSTS1abS9rUy1uanNtTTNqU21zLU1abXR6dGRrcg.webp",
+        testimonialText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.",
+        testimonialAuthor: "Pedro Simões Dias",
+        testimonialJob: "Consultant at FPC Legal"
+    }
+}
+
+const cardColors = {
+    0: {
+        backgroundColor: "#087f5b", 
+        fontColor: "#c3fae8"
+    },
+    1: {
+        backgroundColor: "#1864ab", 
+        fontColor: "#d0ebff"
+    },
+    2: {
+        backgroundColor: "#5f3dc4",
+        fontColor: "#e5dbff"
+    }
+}
+
+// Global Variables
+let counter = 0;
+const totalData = Object.keys(userData).length;
+
+// Functions
+function updateCarousel(counter) {
+    const currentData = userData[counter];
+    const currentColor = cardColors[counter];
+    
+    const carousel = document.querySelector(".carousel");
+    const carouselImage = document.querySelector(".carousel__img");
+    const testimonialText = document.querySelector(".testimonial-text");
+    const testimonialAuthor = document.querySelector(".testimonial-author");
+    const testimonialJob = document.querySelector(".testimonial-job");
+
+    carousel.style.backgroundColor = currentColor.backgroundColor;
+    carouselImage.src = currentData.backgroundImage;
+    testimonialText.textContent = currentData.testimonialText;
+    testimonialAuthor.textContent = currentData.testimonialAuthor;
+    testimonialAuthor.style.color = currentColor.fontColor;
+    testimonialJob.textContent = currentData.testimonialJob;
+    testimonialJob.style.color = currentColor.fontColor;
+}
+
+function previousSlide() {
+    counter = (counter - 1 + totalData) % totalData;
+
+    updateCarousel(counter);
+    console.log("Previous slide:", counter);
+}
+
+function nextSlide() {
+    counter = (counter + 1 + totalData) % totalData;
+
+    updateCarousel(counter);
+    console.log("Next slide:", counter);
+}
